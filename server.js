1a380102b8e999fbd48ff0e6a392d5a38a58c4ad
Quan Huynh <qlhuynh@gmail.com>
2020-03-24 17:26:50 -0700

    initial checkin

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(express.json())
 app.use(require('./routes'))
 
 require('./config')
-  .then(() => app.listen(process.env.PORT || 3000))
+  .then(() => app.listen(process.env.PORT || 3001))
   .catch(e => console.error(e))
