0adaed45198f07b5e383b3e9ecef42e42bf82dd0
Quan Huynh <qlhuynh@gmail.com>
2020-03-25 09:23:25 -0700

    remove unused component

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ import ExpansionPanel from '@material-ui/core/ExpansionPanel'
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails'
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary'
 import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
